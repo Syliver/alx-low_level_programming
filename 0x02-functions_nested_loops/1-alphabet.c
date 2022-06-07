9025224287888082a7b156d75dfbe7af3a0c7af2
Syliver <sylivestergeorge@gmail.com>
2022-06-07 09:37:40 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+ * print_alphabet(void):- prints aplhabets, in lowercase
+ */
+
+void print_alphabet(void)
+{
+
+	char ch;
+
+	ch = 'a';
+
+	while(ch <= 'z')
+	{
+		_putchat(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+
+}
