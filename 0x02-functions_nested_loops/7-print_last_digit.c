371b7c216f0aa974203ba0fa2b8c385893aa85ed
Syliver <sylivestergeorge@gmail.com>
2022-06-08 06:28:44 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
 /**
  * print_last_digit(init)- prints last digit of a number
  * @n: the int to extract the last digit from
- * Return- value of last digit
+ * Return:: value of last digit
  */
 
 int print_last_digit(int n)
